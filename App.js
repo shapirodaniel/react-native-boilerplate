af876f5dcb18c8b76a0eac760e554a924348ecb0
shapirodaniel <shapirodanieladam@gmail.com>
2021-06-26 07:16:18 -0500

    add: joke fetch

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const getJokes = async setter => {
+	fetch('https://official-joke-api.appspot.com/random_joke')
+		.then(res => res.json())
+		.then(json => setter(json))
+		.catch(err => console.error(err));
+};
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [joke, setJoke] = useState({});
+
+	const { setup, punchline } = joke;
+
+	useEffect(() => {
+		let isMounted = true;
+		if (isMounted) getJokes(setJoke);
+		return () => {
+			isMounted = false;
+		};
+	}, []);
+
+	return (
+		<View style={styles.container}>
+			<Text>{setup}</Text>
+			<Text>{punchline}</Text>
+			<StatusBar style='auto' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+		overflow: 'scroll',
+	},
 });
